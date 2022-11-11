a5d7df6b1f38e3af0d08d28fbed09e5ed8c9ba15
Brezzor <oliv095w@edu.zealand.dk>
2022-11-11 09:15:32 +0100

    first push

---
+++ HotelOpgave/DbClient.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelOpgave
+{
+    public static class DbClient
+    {
+        public static void GetAll<T>(SqlConnection con) where T : class
+        {
+            string quary = $"SELECT * FROM {typeof(T).Name}";
+            DataSet ds = new DataSet();
+
+            using (SqlCommand command = new SqlCommand(quary, con))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(ds);                
+
+                foreach (DataTable tables in ds.Tables)
+                {
+                    foreach (DataRow rows in tables.Rows)
+                    {
+                        foreach (DataColumn col in tables.Columns)
+                        {
+                            Console.WriteLine($"{col}: {rows[col]}");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }        
+
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+
+            sqlConnectionStringBuilder.DataSource = "(localdb)\\MSSQLLocalDB";
+            sqlConnectionStringBuilder.InitialCatalog = "HotelDatabase";
+            sqlConnectionStringBuilder.IntegratedSecurity = true;
+            sqlConnectionStringBuilder.ConnectTimeout = 30;
+            sqlConnectionStringBuilder.Encrypt = false;
+            sqlConnectionStringBuilder.TrustServerCertificate = false;
+            sqlConnectionStringBuilder.ApplicationIntent = ApplicationIntent.ReadWrite;
+            sqlConnectionStringBuilder.MultiSubnetFailover = false;
+
+            return sqlConnectionStringBuilder.ConnectionString;
+        }
+    }
+}
--- HotelOpgave/Program.cs
+++ HotelOpgave/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HotelOpgave;
+using HotelOpgave.Models;
+using Microsoft.Data.SqlClient;
+
+using (SqlConnection con = new SqlConnection(DbClient.GetConnectionString()))
+{
+    con.Open();
+    DbClient.GetAll<Hotel>(con);
+    DbClient.GetAll<Room>(con);
+    DbClient.GetAll<Guest>(con);
+    DbClient.GetAll<Booking>(con);
+}
